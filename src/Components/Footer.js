338d94a928ff3cd932800178b1ce1319e860b7d6
meriszecic <zecicmeris@gmail.com>
2023-07-11 16:22:23 +0200

    Adding footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,8 @@
+import '../Style/Footer.css'
+export default function Footer(){
+    return (
+        <>
+        <h1>footer</h1>
+        </>
+    )
+}
--- src/Components/ShopingPage.js
+++ src/Components/ShopingPage.js
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 
@@ -6,6 +7,7 @@ export default function ShopingPage (){
      <>
         <Header></Header>
         <Main></Main>
+        <Footer></Footer>
     </>
     );
 }
